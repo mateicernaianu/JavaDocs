ba356779e98c3b2be53ea83682944ec3082ffa3d
mateicernaianu <matei.cernaianu@gmail.com>
2016-07-05 11:57:28 -0700

    commit

--- Collections/Workshop/warm-up2/src/main/java/exercise3/MyHashMap.java
+++ Collections/Workshop/warm-up2/src/main/java/exercise3/MyHashMap.java
@@ -6,7 +6,7 @@ import java.util.*;
  * Exercise 3. Implement a HashMap from scratch. In order to pass all the tests
  * you need to implement all the methods defined below. The key-value pair will
  * be encapsulated in the MyHashMap.MyEntry object defined below.
- *
+ * <p>
  * The buckets list in which each MyEntry object will be stored is stored in "buckets" object.
  */
 public class MyHashMap {
@@ -20,47 +20,47 @@ public class MyHashMap {
 
         // Initialize buckets list
         buckets = new ArrayList<LinkedList<MyEntry>>();
-        for(Integer i = 0; i < capacity; i++) {
+        for (Integer i = 0; i < capacity; i++) {
             buckets.add(new LinkedList<MyEntry>());
         }
     }
 
     public String get(String key) {
 
-        LinkedList<MyEntry> list ;
-        if(key == null)
+        LinkedList<MyEntry> list;
+        if (key == null)
             list = buckets.get(0);
         else
             list = buckets.get(Math.abs(key.hashCode()) % capacity);
-        for(MyEntry m:list)
-            if(m.key.equals(key))
+        for (MyEntry m : list)
+            if (m.key.equals(key))
                 return m.value;
         return null;
     }
 
     public void put(String key, String value) {
         // TODO
-        MyEntry entry = new MyEntry(key,value);
+        MyEntry entry = new MyEntry(key, value);
 
-        LinkedList<MyEntry> list ;
-        if(key == null)
+        LinkedList<MyEntry> list;
+        if (key == null)
             list = buckets.get(0);
         else
             list = buckets.get(Math.abs(key.hashCode()) % capacity);
         int ok = 0;
-        for(MyEntry m:list)
-            if(m.key.equals(key)) {
+        for (MyEntry m : list)
+            if (m.key.equals(key)) {
                 m.value = value;
                 ok = 1;
             }
-        if(ok == 0)
+        if (ok == 0)
             list.add(entry);
     }
 
     public Set<String> keySet() {
         Set<String> set = new HashSet<String>();
-        for( LinkedList<MyEntry> i : buckets){
-            for(MyEntry j : i){
+        for (LinkedList<MyEntry> i : buckets) {
+            for (MyEntry j : i) {
                 set.add(j.getKey());
             }
         }
@@ -69,8 +69,8 @@ public class MyHashMap {
 
     public Collection<String> values() {
         ArrayList<String> list = new ArrayList<String>();
-        for( LinkedList<MyEntry> i : buckets){
-            for(MyEntry j : i){
+        for (LinkedList<MyEntry> i : buckets) {
+            for (MyEntry j : i) {
                 list.add(j.getValue());
             }
         }
@@ -78,15 +78,15 @@ public class MyHashMap {
     }
 
     public String remove(String key) {
-        if(key == null){
+        if (key == null) {
             return null;
         }
         if (capacity == 0)
             return null;
-        int k = Math.abs(key.hashCode())%capacity;
+        int k = Math.abs(key.hashCode()) % capacity;
         LinkedList<MyEntry> list = buckets.get(k);
-        for(MyEntry m:list)
-            if(m.key.equals(key)) {
+        for (MyEntry m : list)
+            if (m.key.equals(key)) {
                 list.remove(m);
                 return m.getValue();
             }
@@ -96,10 +96,10 @@ public class MyHashMap {
     public boolean containsKey(String key) {
         if (capacity == 0)
             return false;
-        int k = Math.abs(key.hashCode())%capacity;
+        int k = Math.abs(key.hashCode()) % capacity;
         LinkedList<MyEntry> list = buckets.get(k);
-        for(MyEntry m:list)
-            if(m.key.equals(key)) {
+        for (MyEntry m : list)
+            if (m.key.equals(key)) {
                 return true;
             }
         return false;
@@ -108,9 +108,13 @@ public class MyHashMap {
     public boolean containsValue(String value) {
         // TODO
         for (LinkedList<MyEntry> le : buckets) {
-            for (MyEntry var:le){
+            for (MyEntry var : le) {
+
                 if (var.value.equals(value))
                     return true;
+                if (var.equals(value))
+                    return true;
+
             }
         }
 
@@ -119,9 +123,9 @@ public class MyHashMap {
 
     public int size() {
         // TODO Return the number of the Entry objects stored in all the buckets
-        int count=0;
+        int count = 0;
         for (LinkedList<MyEntry> le : buckets) {
-            for (MyEntry var:le){
+            for (MyEntry var : le) {
                 count++;
             }
         }
@@ -137,8 +141,8 @@ public class MyHashMap {
 
     public Set<MyEntry> entrySet() {
         Set<MyEntry> set = new HashSet<MyEntry>();
-        for(LinkedList<MyEntry> me : buckets) {
-            for(MyEntry m:me)
+        for (LinkedList<MyEntry> me : buckets) {
+            for (MyEntry m : me)
                 set.add(m);
         }
         // TODO Return a Set containing all the Entry objects
@@ -147,8 +151,8 @@ public class MyHashMap {
 
     public boolean isEmpty() {
         // TODO
-        for(LinkedList<MyEntry> me : buckets) {
-            if(!me.isEmpty())
+        for (LinkedList<MyEntry> me : buckets) {
+            if (!me.isEmpty())
                 return false;
         }
         return true;
